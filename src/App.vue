643fe641daa4e29d014ff3a3d0fe60a0cf4bd62c
marcinhojazz <marcinhojazz@gmail.com>
2023-10-18 19:03:54 -0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <TheHeader 
+    v-show="showHeader"
+  />
+  <div v-show="showName">
+    Nome: {{ firstName }}
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'">Usuário Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Usuário Marketing</div>
+  <div v-else>Usuário Marketing</div>
+  
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: 'Márcio',
+      lastName: 'Machado',
+      showName: false,
+      acessLevel: 'admin'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 60px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,100 @@
+<template>
+  
+  <h1 :class="{ 'title': true, 'title-home': isHome }">
+    Curso Vue 3
+  </h1>
+
+  <p>
+    
+  </p>
+  
+  
+  <div 
+    v-for="(obj, index) in todos"
+    :key="obj.id"
+    class="todos-item"
+  >
+    <img 
+      v-if="obj.imgSrc"
+      :src="obj.imgSrc"
+    />
+    
+    {{ index }} - {{ obj.title }}
+    
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      isHome: false,
+      classVar: 'title',
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false,
+          "imgSrc": "https://via.placeholder.com/150"
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false,
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 60px;
+}
+
+.todos-item {
+  background: #000;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: #fff;
+  border-radius: 4px;
+}
+
+.title-home {
+  font-size: 40px;
+  color: greenyellow;
+}
+
+.title {
+  font-size: 20px;
+  color: blue;
+}
+</style>
